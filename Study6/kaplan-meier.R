bb2f70d6278b8fae2513fc583af4668a27b91b40
clairecoffey <clairecoffey@live.co.uk>
2025-10-29 16:09:14 +0100

    Adding logrank test to kaplan-meier results

--- Study6/kaplan-meier.R
+++ Study6/kaplan-meier.R
@@ -1,6 +1,17 @@
+install.packages("survival")
+install.packages("survminer")
+install.packages("dplyr")
+install.packages("svglite")
+
 library(survival)
 library(survminer)
 library(dplyr)
+
+combined <- read.csv("S:/AG/f-dhzc-profid/Data Transfer to Charite/combined_dataset.csv")
+
+data_dir <- "S:/AG/f-dhzc-profid/Data Transfer to Charite"
+setwd("T:/Dokumente/PROFID/Study6")
+
 
 combined_KM <- combined %>%
   mutate(
@@ -27,12 +38,6 @@ km_plot <- ggsurvplot(
   ggtheme = theme_minimal()
 )
 
-logrank_test <- survdiff(Surv(Survival_time, Status_KM) ~ BMI_cat, data = combined_KM)
-logrank_test
-
-p_value <- 1 - pchisq(logrank_test$chisq, length(logrank_test$n) - 1)
-p_value
-
 ggsave(
   filename = "KM_Survival_by_BMI.svg",
   plot = km_plot$plot,
@@ -45,4 +50,21 @@ km_plot
 
 summary_fit <- summary(fit_KM)
 write.csv(as.data.frame(summary_fit), "KM_Survival_Summary.csv", row.names = FALSE)
+
+
+logrank_test <- survdiff(Surv(Survival_time, Status_KM) ~ BMI_cat, data = combined_KM)
+logrank_test
+
+
+# Extract results
+logrank_results <- data.frame(
+  chi_squared = logrank_test$chisq,
+  degrees_of_freedom = length(logrank_test$n) - 1,
+  p_value = 1 - pchisq(logrank_test$chisq, length(logrank_test$n) - 1)
+)
+p_value
+
+# Write to CSV
+write.csv(logrank_results, "KM_LogRank_Test.csv", row.names = FALSE)
+
 
