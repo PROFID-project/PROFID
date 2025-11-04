c45e1f7afb4c4320f0205fbe2bf0e5f799967293
clairecoffey <clairecoffey@live.co.uk>
2025-11-04 16:47:07 +0100

    Imputing only relevant variables (for speed)

--- Study6/MICE.R
+++ Study6/MICE.R
@@ -49,15 +49,15 @@ impute_vars <- c(
   "ACE_inhibitor_ARB", "Beta_blockers", "Lipid_lowering",
   "Revascularisation_acute",
   
-  #  auxiliary variables
+  #  auxiliary variables (extended model)
   "Cholesterol", "LDL", "HDL", "Triglycerides",
   "Stroke_TIA", 
-  "Anti_platelet", "Anti_coagulant", "Diuretics", "Anti_anginal", "Calcium_antagonists",
-  "Aldosterone_antagonist", "Digitalis_glycosides",
-  "PCI_acute", "PCI", "CABG_acute", "CABG", "Thrombolysis_acute",
-  "LBBB", "RBBB", "AF_atrial_flutter",
-  "Baseline_type", "HasMRI", "CVD_risk_region", 
-  "Anti_diabetic", "Anti_diabetic_oral", "Anti_diabetic_insulin",
+  # "Anti_platelet", "Anti_coagulant", "Diuretics", "Anti_anginal", "Calcium_antagonists",
+  # "Aldosterone_antagonist", "Digitalis_glycosides",
+  # "PCI_acute", "PCI", "CABG_acute", "CABG", "Thrombolysis_acute",
+  # "LBBB", "RBBB", "AF_atrial_flutter", "HasMRI",
+   "Baseline_type", "CVD_risk_region", 
+  # "Anti_diabetic", "Anti_diabetic_oral", "Anti_diabetic_insulin",
   "MI_type"
 )
 
@@ -70,12 +70,12 @@ categorical_vars <- c(
   
   #  auxiliary variables
   "Stroke_TIA", "HF",
-  "Anti_platelet", "Anti_coagulant", "Diuretics", "Anti_anginal", "Calcium_antagonists",
-  "Aldosterone_antagonist", "Digitalis_glycosides",
-  "PCI_acute", "PCI", "CABG_acute", "CABG", "Thrombolysis_acute",
-  "LBBB", "RBBB", "AF_atrial_flutter",
-  "Baseline_type", "HasMRI", "CVD_risk_region", 
-  "Anti_diabetic", "Anti_diabetic_oral", "Anti_diabetic_insulin",
+  # "Anti_platelet", "Anti_coagulant", "Diuretics", "Anti_anginal", "Calcium_antagonists",
+  # "Aldosterone_antagonist", "Digitalis_glycosides",
+  # "PCI_acute", "PCI", "CABG_acute", "CABG", "Thrombolysis_acute",
+  # "LBBB", "RBBB", "AF_atrial_flutter", "HasMRI",
+  "Baseline_type", "CVD_risk_region", 
+  # "Anti_diabetic", "Anti_diabetic_oral", "Anti_diabetic_insulin",
   "MI_type"
 )
 
